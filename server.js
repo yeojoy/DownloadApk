f414a54e9ee2fd4f29a8833705d024c9c97d29cf
yeojoy <kim.yeojoy@gmail.com>
2021-10-08 16:08:07 -0400

    update json response
    and remove .apk files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-// const ip = "192.168.1.40"
-const ip = "10.0.0.212"
+const ip = "192.168.1.40"
+// const ip = "10.0.0.212"
 const port = 3000;
 
 app.use(express.static('public'));
@@ -9,8 +9,14 @@ app.use(express.static('public'));
 app.get('/info', function(req, res) {
     res.json({
         version: "1.0.0",
-        versionCode: 2,
-        downloadLink: "http://" + ip + ":" + port + "/download"
+        versionCode: 1,
+        downloadLink: "http://" + ip + ":" + port + "/download",
+        venueName: "LiveBarn Office - Montreal",
+        id: "yeojong",
+        pwd: "live1234",
+        host: "192.168.1.19",
+        prefix: "/live%d.sdp",
+        indices: [2, 3]
     });
 });
 
